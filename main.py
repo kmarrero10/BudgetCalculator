bb9736703039d5a8929afb1fc7124ca2bf6c5a25
Kyle Marrero <kylemarrero@Kyles-MacBook-Pro.local>
2019-08-07 07:49:30 -0500

    started implementing budget stuff

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 
-	content = db.Column(db.String(200), nullable=False)
+	income = db.Column(db.Integer, default=0, nullable=False)
+	expense = db.Column(db.String(200), nullable=False)
+	cost = db.Column(db.Integer, default=0, nullable=False)
 	completed = db.Column(db.Integer, default=0)
 	date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -20,48 +22,49 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-	if request.method == 'POST':
-		task_content = request.form['content']
-		new_task = Todo(content=task_content)
+	return render_template("home.html")
+# 	if request.method == 'POST':
+# 		task_content = request.form['content']
+# 		new_task = Todo(content=task_content)
 
-		try:
-			db.session.add(new_task)
-			db.session.commit()
-			return redirect('/')
+# 		try:
+# 			db.session.add(new_task)
+# 			db.session.commit()
+# 			return redirect('/')
 
-		except:
-			return 'There was an issue adding your task!'
+# 		except:
+# 			return 'There was an issue adding your task!'
 
-	else:
-		tasks = Todo.query.order_by(Todo.date_created).all()
-		return render_template("home.html", tasks=tasks)
+# 	else:
+# 		tasks = Todo.query.order_by(Todo.date_created).all()
+# 		return render_template("home.html", tasks=tasks)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-	task_to_delete = Todo.query.get_or_404(id)
+# @app.route('/delete/<int:id>')
+# def delete(id):
+# 	task_to_delete = Todo.query.get_or_404(id)
 
-	try:
-		db.session.delete(task_to_delete)
-		db.session.commit()
-		return redirect('/')
-	except:
-		return 'There was a problem deleting that task'
+# 	try:
+# 		db.session.delete(task_to_delete)
+# 		db.session.commit()
+# 		return redirect('/')
+# 	except:
+# 		return 'There was a problem deleting that task'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-	task = Todo.query.get_or_404(id)
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+# 	task = Todo.query.get_or_404(id)
 
 
-	if request.method == 'POST':
-		task.content = request.form['content']
+# 	if request.method == 'POST':
+# 		task.content = request.form['content']
 
-		try:
-			db.session.commit()
-			return redirect('/')
-		except:
-			return 'There was an issue updating your task!'
-	else:
-		return render_template('update.html', task=task)
+# 		try:
+# 			db.session.commit()
+# 			return redirect('/')
+# 		except:
+# 			return 'There was an issue updating your task!'
+# 	else:
+# 		return render_template('update.html', task=task)
 
 
 @app.route("/calculator")
